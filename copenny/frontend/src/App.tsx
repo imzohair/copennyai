781e70345d06e1eb857a2e8b89fb7c2ef6610ca3
Mirza Zohair <mirzazohair@Mac.mshome.net>
2026-09-04 16:26:20 +0530

    Fix flex layout in App.tsx

--- copenny/frontend/src/App.tsx
+++ copenny/frontend/src/App.tsx
@@ -4,13 +4,13 @@ import { Timeline } from './components/Timeline';
 
 function App() {
   return (
-    <>
+    <div className="flex h-screen w-full overflow-hidden">
       <Sidebar />
       <div className="flex-1 flex flex-col h-screen overflow-hidden bg-[#121212]">
         <Topbar />
         <Timeline />
       </div>
-    </>
+    </div>
   );
 }
 
